6412bc775c5d121432894e65fd843b546a0c5959
Brandon Trump <brandontrump101@gmail.com>
2023-06-01 23:15:22 +1200

    feat: created card to display data fetched from advice api

---
+++ src/components/AdviceCard.tsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import "../styles/AdviceCard.css";
+import dice from "../assets/dice.svg";
+
+type Advice = {
+  id: number;
+  advice: string;
+};
+
+const AdviceCard = () => {
+  const [advice, setAdvice] = useState<Advice>({ id: 0, advice: "" });
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    let isCancelled = false;
+    fetch("	https://api.adviceslip.com/advice")
+      .then((response) => response.json())
+      .then((data) => {
+        if (isCancelled) {
+          setAdvice(data.slip);
+          console.log(data.slip);
+          setIsLoading(false);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    return () => {
+      isCancelled = true;
+    };
+  }, []);
+
+  return (
+    <div className="card-container">
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <div className="card-content">
+            <h3>Advice #{advice.id}</h3>
+            <h1>"{advice.advice}"</h1>
+          </div>
+          <div className="advice-line"></div>
+          <div className="re-roll-cta">
+            <img src={dice} alt="dice" />
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default AdviceCard;
